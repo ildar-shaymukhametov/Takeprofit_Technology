effbc938b3e90805231ef647cab74cb8cbe9f6ed
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-03-26 18:32:59 +0500

    done

--- Program.cs
+++ Program.cs
@@ -1,54 +1,43 @@
 ﻿using System.Net.Sockets;
 
-var tasks = Enumerable.Range(1, 2013).Select(CreateTask).ToArray();
+var tasks = Enumerable.Range(1, 2013).Select(GetNumberAsync).ToArray();
 var numbers = await Task.WhenAll(tasks);
-
-if (numbers.Length != 2013)
-{
-    throw new Exception($"Wrong number of items: {numbers.Length}");
-}
-
 var median = CalculateMedian(numbers);
 
-Console.WriteLine($"Median: {median}");
+Console.WriteLine($"Median: {median}"); // 4925512
 
-async Task<int> CreateTask(int number)
+async Task<int> GetNumberAsync(int number)
 {
     var response = await GetValidResponseAsync(number);
-    var numbers = new string(response.Where(char.IsDigit).ToArray());
-    var result = int.Parse(numbers);
-
-    System.Console.WriteLine($"{number}: {result}");
-    return result;
+    var numberString = new string(response.Where(char.IsDigit).ToArray());
+    return int.Parse(numberString);
 }
 
 async Task<string> GetValidResponseAsync(int number)
 {
     while (true)
     {
-        var response = await SendNumberAsync(number);
-        var ok = response != null && response.Contains("\n");
+        var result = await SendDataAsync(number);
+        var ok = result != null && result.Contains("\n");
         if (ok)
         {
-            return response;
+            return result;
         }
     }
 }
 
-async Task<string?> SendNumberAsync(int number)
+async Task<string?> SendDataAsync(int data)
 {
     try
     {
         using var client = new TcpClient("88.212.241.115", 2013);
         using var stream = client.GetStream();
         using var writer = new StreamWriter(stream);
-        await writer.WriteAsync($"{number}\n");
+        await writer.WriteAsync($"{data}\n");
         await writer.FlushAsync();
 
         using var reader = new StreamReader(stream);
-        var result = await reader.ReadToEndAsync();
-
-        return result;
+        return await reader.ReadToEndAsync();
     }
     catch
     {
@@ -59,9 +48,5 @@ async Task<string?> SendNumberAsync(int number)
 static int CalculateMedian(IEnumerable<int> list)
 {
     var sorted = list.OrderBy(x => x).ToArray();
-    var a = sorted[sorted.Length / 2];
-    var b = sorted[sorted.Length / 2 + 1];
-    var result = (a + b) / 2;
-
-    return result;
+    return sorted[sorted.Length / 2];
 }
